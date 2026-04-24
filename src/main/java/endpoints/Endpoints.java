2d6adc4ce3f78a904e839fbd0f730f55b7f5a8c1
tamilamirtharajj <ironknightprime@gmail.com>
2026-04-24 16:46:13 +0530

    Added Record  module

--- src/main/java/endpoints/Endpoints.java
+++ src/main/java/endpoints/Endpoints.java
@@ -2,9 +2,8 @@ package endpoints;
 
 public class Endpoints {
 
-    public static final String CREATE_USER = "/api/users";
-    public static final String GET_USERS = "/api/users?page=1";
-    public static final String GET_USERS_BY_PAGE = "/api/users?page=";
-    public static final String GET_USER_BY_ID = "/api/users/";
-    public static final String INVALID_USERS = "/api/users/999";
+    public static final String CREATE_RECORD = "/api/collections/orders/records";
+    public static final String GET_RECORD = "/api/collections/orders/records";
+    public static final String UPDATE_RECORD = "/api/collections/orders/records";
+    public static final String DELETE_RECORD = "/api/collections/orders/records";
 }
