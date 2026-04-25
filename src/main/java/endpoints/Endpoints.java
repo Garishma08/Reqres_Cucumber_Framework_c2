36c3584afc89cb29c584944f3262b1008e093a9f
jaiarunk <jaiarunk29042004@gmail.com>
2026-04-25 09:56:47 +0530

    Auth module: added step definitions, excel validation, test runner and endpoints

--- src/main/java/endpoints/Endpoints.java
+++ src/main/java/endpoints/Endpoints.java
@@ -7,4 +7,7 @@ public class Endpoints {
     public static final String GET_USERS_BY_PAGE = "/api/users?page=";
     public static final String GET_USER_BY_ID = "/api/users/";
     public static final String INVALID_USERS = "/api/users/999";
+    public static final String REGISTER = "/api/register";
+    public static final String LOGIN = "/api/login";
+    public static final String LIST_COLORS = "/api/unknown";
 }
