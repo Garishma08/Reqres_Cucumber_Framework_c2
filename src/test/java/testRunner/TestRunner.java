f48fdbeb18a1c04d12c3f20d5ef1d0961a2baf9b
Garishma <garishmaraju0803@gmail.com>
2026-04-25 09:18:00 +0530

    Added Common Runner

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -3,8 +3,10 @@ package testRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinitions", "hooks" }, plugin = {
-                "pretty" }, monochrome = true)
-public class TestRunner
-                extends AbstractTestNGCucumberTests {
+@CucumberOptions(features = "src/test/resources/feature", glue = { "stepdefinition", "hooks" }, plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"
+}, monochrome = true)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
