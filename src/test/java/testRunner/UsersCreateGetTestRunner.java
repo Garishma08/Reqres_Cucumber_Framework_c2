f135312a5e864e715259e7ccf1a756dd08d289b4
Garishma <garishmaraju0803@gmail.com>
2026-04-25 08:29:43 +0530

    changes in config

--- src/test/java/testRunner/UsersCreateGetTestRunner.java
+++ src/test/java/testRunner/UsersCreateGetTestRunner.java
@@ -3,11 +3,11 @@ package testRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/feature/Users_create-get.feature", glue = { "stepdefinition",
-        "hooks" }, plugin = {
-                "pretty",
-                "html:target/reports/users-createget-report.html",
-                "json:target/reports/users-createget-report.json"
-        }, monochrome = true, tags = "@users_create_get")
+@CucumberOptions(features = "src/test/resources/feature/Users_create-get.feature", glue = { "stepdefinition.users",
+                "hooks" }, plugin = {
+                                "pretty",
+                                "html:target/reports/users-createget-report.html",
+                                "json:target/reports/users-createget-report.json"
+                }, monochrome = true, tags = "@users_create_get")
 public class UsersCreateGetTestRunner extends AbstractTestNGCucumberTests {
 }
