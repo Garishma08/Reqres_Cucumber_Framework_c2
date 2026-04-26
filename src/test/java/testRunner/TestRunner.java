aa52dfe1643f559f94eed82ba8c63ae7df8510ad
Garishma <garishmaraju0803@gmail.com>
2026-04-26 18:05:58 +0530

    Added extendt files for report generation

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,9 +4,10 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/feature", glue = { "stepdefinition", "hooks" }, plugin = {
-                "pretty",
-                "html:target/cucumber-report.html",
-                "json:target/cucumber.json"
+        "pretty",
+        "html:target/cucumber-report.html",
+        "json:target/cucumber.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
 }, monochrome = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
